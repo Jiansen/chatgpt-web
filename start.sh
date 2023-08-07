6a252c59fc8a6681e60d9c99bb58cc676a67566f
Jiansen <jiansenhe@gmail.com>
2023-08-07 06:08:02 +0000

    feat: config update

--- start.sh
+++ start.sh
@@ -1,11 +1,24 @@
+xtimestamp() {
+  date +"[%Y-%m-%d %H:%M:%S]"
+}
+
+#pnpm_start_command="pnpm start"
 
 cd ./service
-nohup pnpm start > service.log &
+# Use 'stdbuf' to disable output buffering for real-time log appending
+# nohup sh -c "stdbuf -i0 -o0 -e0 pnpm start 2>&1 | while IFS= read -r line; do printf '%s %s\n' \"$(timestamp)\" \"\$line\"; done" >> service_log.log &
+# nohup sh -c "stdbuf -i0 -o0 -e0 pnpm start 2>&1 | while IFS= read -r line; do printf '%s %s\n' \"[$(date '+%Y-%m-%d %H:%M:%S')]\" \"\$line\"; done" >> service_log.log &
+nohup bash -c 'stdbuf -i0 -o0 -e0 $0 2>&1 | while IFS= read -r line; do printf "%s %s\n" "[$(date "+%Y-%m-%d %H:%M:%S")] " "$line"; done' "pnpm start" >> service_log.log &
+
+# nohup pnpm start >> service_log.log &
 echo "Start service complete!"
 
 
 cd ..
-echo "" > front.log
-nohup pnpm dev > front.log &
+echo "" >> front.log
+# nohup sh -c "stdbuf -i0 -o0 -e0 sudo npm run dev 2>&1 | while IFS= read -r line; do printf '%s %s\n' \"$(timestamp)\" \"\$line\"; done" >> front.log &
+# nohup sh -c "stdbuf -i0 -o0 -e0 sudo npm run dev 2>&1 | while IFS= read -r line; do printf '%s %s\n' \"[$(date '+%Y-%m-%d %H:%M:%S')]\"  \"\$line\"; done" >> front.log &
+nohup bash -c 'stdbuf -i0 -o0 -e0 $0 2>&1 | while IFS= read -r line; do printf "%s %s\n" "[$(date "+%Y-%m-%d %H:%M:%S")] " "$line"; done' "sudo npm run dev" >> front.log &
+# nohup pnpm dev >> front.log &
 echo "Start front complete!"
 tail -f front.log
